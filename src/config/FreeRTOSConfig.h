ba36e723f485582db7b5d3e8ac42e08b2c3bb461
Ryan David <ryan@redline-electronics.com>
2012-01-13 23:15:08 -0500

    Fix for application hang

--- src/config/FreeRTOSConfig.h
+++ src/config/FreeRTOSConfig.h
@@ -62,7 +62,7 @@
  *----------------------------------------------------------*/
 
 #define configUSE_PREEMPTION      1
-#define configUSE_IDLE_HOOK       1
+#define configUSE_IDLE_HOOK       0
 #define configUSE_TICK_HOOK       0
 #define configCPU_CLOCK_HZ        ( APPL_CPU_SPEED ) //( FOSC0 ) /* Hz clk gen */
 #define configPBA_CLOCK_HZ        ( APPL_PBA_SPEED )
